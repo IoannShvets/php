79fd3bab3b53594b57a8cf975c731d60058f053c
IoannShvets <shvets.ioann2015@yandex.ru>
2015-12-28 22:33:50 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<?php
+    if($_FILES["filename"]["size"] > 1024*3*1024){
+        echo ("Размер файла превышает три мегабайта");
+        exit;
+    } 
+    if(is_uploaded_file($_FILES["filename"]["tmp_name"])) {
+    move_uploaded_file($_FILES["filename"]["tmp_name"], "images/".$_FILES["filename"]["name"]);
+    include('classSimpleImage.php');
+    $image = new SimpleImage();
+    $image->load("images/".$_FILES["filename"]["name"]);
+    $image->resize(2, 2);
+    $image->save('image4.jpg');
+    $image2 = imageCreateFromJpeg('image4.jpg');
+    $color = imagecolorat($image2, 1, 1);
+    $r = ($color >> 16) & 0xFF;
+    $g = ($color >> 8) & 0xFF;
+    $b = $color & 0xFF;
+    // echo $r."<br />";
+    // echo $g."<br />";
+    //echo $b."<br />";
+    echo "<img src = 'images/".$_FILES["filename"]["name"]."' >" . "<br />";
+    $color_echo = (($r * 299) + ($g * 587) + ($b * 114)) / 1000;
+    if ($color_echo <= 125){
+        echo "<span style='width: 400px; height: 400px; background-color: #323232; color: #fff;'> . Black text " . $color_echo . "</span>";
+    } else {
+        echo "White text $color_echo";
+    }   
+} else {
+    echo("Ошибка загрузки файла");
+   }
+?>
+</body>
+</html>
