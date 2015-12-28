64365151e5d55e6bc1226fb7be1e3b6ccbe75f02
IoannShvets <shvets.ioann2015@yandex.ru>
2015-12-28 22:33:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>Загрузка файлов на сервер</title>
+</head>
+<body>
+      <form action="test.php" method="post" enctype="multipart/form-data">
+      <input type="file" name="filename"><br> 
+      <input type="submit" value="Upload"><br>
+      </form>
+</body>
+</html>
