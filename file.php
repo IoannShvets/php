b0d675c595402cc296bc3ece8bccb34e0e1123fe
IoannShvets <shvets.ioann2015@yandex.ru>
2016-01-11 13:02:03 +0300

    Update file.php

--- file.php
+++ file.php
@@ -3,12 +3,14 @@ require 'connect.php';
 
 $ip_adress = $_SERVER['REMOTE_ADDR']; // узнаем IP
 
-$browser = get_browser(null, true); // узнаем браузер
+echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
+$browser = get_browser(null, true);
+print_r($browser);
 
-$name = trim($_REQUEST['name']); // считываем данные с поля name
+$name = trim($_REQUEST['name']); // считываем данные с поля
 
-$insert_sql = "INSERT INTO guestbook (ip, browser, name)" . "VALUES('$ip_adress', '$browser', '$name');";
-mysql_query($insert_sql); // загружаем в базу 
+$insert_sql = "INSERT INTO guestbook (ip_adress, browser, name)" . "VALUES('{$ip_adress}', "$_SERVER['HTTP_USER_AGENT']" , '{$name}');"; 
+mysql_query($insert_sql);  // загружаем в базу данныъ
 
-echo "Ip, Browser, Name added to table";
+echo "Ip, Browser, Name added to table"; // сообщение об успешной доставке
 ?>
