a91af3a65bbb92a740f4117c1262d8b9cf45a4e3
IoannShvets <shvets.ioann2015@yandex.ru>
2016-01-11 12:44:08 +0300

    file.php
    file.php

---
+++ file.php
@@ -0,0 +1,14 @@
+<?php 
+require 'connect.php';
+
+$ip_adress = $_SERVER['REMOTE_ADDR']; // узнаем IP
+
+$browser = get_browser(null, true); // узнаем браузер
+
+$name = trim($_REQUEST['name']); // считываем данные с поля
+
+$insert_sql = "INSERT INTO guestbook (ip, browser, name)" . "VALUES('$ip_adress', '$browser', '$name');";
+mysql_query($insert_sql); //
+
+echo "Ip, Browser, Name added to table";
+?>
